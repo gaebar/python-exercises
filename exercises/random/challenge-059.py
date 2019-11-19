5c3c2ebf5147d11179dea7c7188c8db8a623f03e
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-11-19 15:58:42 +0000

    Update exercises/random/challenge-059.py
    Co-Authored-By: Robert Rees <rrees@users.noreply.github.com>

--- exercises/random/challenge-059.py
+++ exercises/random/challenge-059.py
@@ -5,7 +5,7 @@
 Display five colours and ask the user to pick one. 
 If they pick the same as the program has chosen, say "Well done", 
 otherwise display a witty answer which involves the correct colour, e.g. "I bet you are GREEN with envy" or 
-"Your probably felling BLUE right now". Ask them to guess again;
+"Your probably feeling BLUE right now". Ask them to guess again;
 if they have still not got it right, keep giving them the same clue 
 and ask the user to enter a colour until they guess it correctly.
 """
