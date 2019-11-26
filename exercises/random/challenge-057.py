ff077777489045c69ca63e4a102dbd9a24dc3bdc
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-11-26 19:24:15 +0000

    Completed challenge 57

--- exercises/random/challenge-057.py
+++ exercises/random/challenge-057.py
@@ -8,16 +8,22 @@ if they are too high or low before they pick again.
 
 # Play around to understand more: added a different message if user guess doesn't match.
 import random
+
 computer_choice = random.randint(1, 10)
 correct = False
-user_choice = int(input("Pick a number between 1 and 10: "))
-while correct == False:
-    user_choice = int(input("Pick another number: "))
-    if user_choice == computer_choice:
-        correct == True
-        print("You rock! Computer choice matched", int(computer_choice))
-        break
-    elif user_choice > computer_choice:
-        print("Too high")
-    else:
-        print("Too low")
+
+
+def check_user_choice():
+    while correct == False:
+        user_choice = int(input("Pick a number between 1 and 10: "))
+        if user_choice == computer_choice:
+            correct == True
+            print("You rock! Computer choice matched", int(computer_choice))
+            return
+        elif user_choice > computer_choice:
+            print("Too high")
+        else:
+            print("Too low")
+
+
+check_user_choice()
