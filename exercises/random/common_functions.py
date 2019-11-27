f2e43fe4c008d61709038f54f46cb63c8f879f25
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-11-27 00:23:40 +0000

    Completed challenge 58

--- exercises/random/common_functions.py
+++ exercises/random/common_functions.py
@@ -1,4 +1,4 @@
-# Ask for user input and ensuring the user entering an integer
+# Ask for user input and ensures the user enters an integer
 def get_user_input(message):
     while True:
         user_input = input(message)
@@ -8,21 +8,21 @@ def get_user_input(message):
             return int(user_input)
 
 
+# Ask for user input and ensures the user enters a float
 def get_user_input_float(message):
     while True:
         user_input = input(message)
-        if not isinstance(eval(user_input), float):
-            print("Oops! That was not a valid number. Try again...")
-        else:
+
+        if is_float(user_input):
             return float(user_input)
+        else:
+            print("Oops! That was not a valid number. Try again...")
 
 
-# # TRY: Ask for user input and ensuring the user entering an integer or a float number
-# def get_user_input(message):
-#     while True:
-#         user_input = input(message)
-#         if not user_input.isdigit() or :
-#             print("Oops! That was not a valid number. Try again...")
-#         else:
-#             return int(user_input)
-
+# Check if the input string is a float
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except:
+        return False
