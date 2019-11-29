0b5dfd71f4e707dce10f9e690cba618e566f52d9
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-11-29 00:12:45 +0000

    Added a string join, removed tail else and rewrote challenge 72 to include error input condition

--- exercises/tuples-lists-dictionaries/challenge_070.py
+++ exercises/tuples-lists-dictionaries/challenge_070.py
@@ -5,9 +5,9 @@
 70: Add to program 069 to ask the user a number and display the country in that position.
 """
 
-country_names = ("England", "Italy", "Spain", "France", "Portugal")
+country_names = ("England", "Italy", "Spain", "France", "Portugal", "Germany")
 
-print(country_names)
+print(", ".join(country_names))
 
 
 # Ask for user input and ensures the user enters an integer
@@ -16,26 +16,29 @@ def get_user_input_digit(message):
         user_input = input(message)
         if not user_input.isdigit():
             print("Oops! That was not a valid number. Try again...")
-        else:
-            return int(user_input)
+        return int(user_input)
 
 
 def ask_for_country_by_index():
+    max_index = len(country_names) - 1
     while True:
-        user_index_choice = get_user_input_digit("Enter a number between 0 and 4: ")
-        if user_index_choice >= len(country_names) - 1:
+        user_index_choice = get_user_input_digit(
+            f"Enter a number between 0 and {max_index}: "
+        )
+        if user_index_choice > max_index:
             print("This number is too high")
-        else:
-            return (user_index_choice, country_names[user_index_choice])
+        return (user_index_choice, country_names[user_index_choice])
 
 
-def print_msg(user_index, user_country):
+def print_message(user_index, user_country):
     print(f"The country with index {user_index} is {user_country}.")
 
 
 def main():
     (user_index, user_country) = ask_for_country_by_index()
-    print_msg(user_index, user_country)
+    print_message(user_index, user_country)
 
 
-main()
+if __name__ == "__main__":
+    main()
+
--- exercises/tuples-lists-dictionaries/challenge_072.py
+++ exercises/tuples-lists-dictionaries/challenge_072.py
@@ -6,9 +6,28 @@ Delete the subject they have chosen from the list before you display the list ag
 """
 
 subjects_list = ["Maths", "Literature", "Science", "History", "Spanish", "Computing"]
-print(subjects_list)
-dislike = input("Which of these subjects do you dislike? ")
-erase_from_list = subjects_list.index(dislike)
-del subjects_list[erase_from_list]
-print(subjects_list)
+
+
+def join_subjects(subjects_list):
+    return ", ".join(subjects_list)
+
+
+print(f"Subjects list: {join_subjects(subjects_list)}")
+
+
+def ask_user_input():
+    while True:
+        user_input = input("Which one of these subjects do you dislike? ")
+        if user_input in subjects_list:
+            erase_from_list = subjects_list.index(user_input)
+            del subjects_list[erase_from_list]
+
+            print(f"{user_input} is no more in the subject list!")
+            print(f"New subjects list: {join_subjects(subjects_list)}")
+            return
+
+        print(f"{user_input} is not in the list. Try again!")
+
+
+ask_user_input()
 
