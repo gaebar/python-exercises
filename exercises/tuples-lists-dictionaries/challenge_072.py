7d92f23f00e9dc51b678ed738d35d440ed866a78
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-12-01 16:04:58 +0000

    removed redundant comment

--- exercises/tuples-lists-dictionaries/challenge_072.py
+++ exercises/tuples-lists-dictionaries/challenge_072.py
@@ -5,9 +5,6 @@ Create a list of six school subjects. Ask the user which of these subjects they 
 Delete the subject they have chosen from the list before you display the list again.
 """
 
-# Penso che voglia dire che l'invalid imput sia se uno scrive History con la h piccola.
-# Dunque aggiungere  l'opzione
-
 subjects_list = ["Maths", "Literature", "Science", "History", "Spanish", "Computing"]
 subjects_list_lowercase = [subject.lower() for subject in subjects_list]
 
