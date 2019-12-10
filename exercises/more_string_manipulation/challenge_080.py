9d021183c569764c5731b5c4fdce8bc555533456
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-12-10 23:49:58 +0000

    completed challenge 80, with test

---
+++ exercises/more_string_manipulation/challenge_080.py
@@ -0,0 +1,47 @@
+# MORE STRING MANIPULATION
+
+# Challenge 080
+"""
+Ask the user to enter their first name and then display the length of their first name.
+Then ask for their surname and display the length of their username. Join their first name
+and surname together with a space between and display the result.
+Finally, display the length of their full name (including the space).
+"""
+
+# user_first_name = input("Please, enter your name: ")
+# print(f"Your name has {len(user_first_name)} characters in it.")
+# user_surname = input("Enter your surname: ")
+# print(f"Your surname has {len(user_surname)} characters in it.")
+# user_name = user_first_name + " " + user_surname
+# print(f"Your full name is {user_name} and has {len(user_name)-1} characters in it")
+
+
+def processing(first_name, last_name):
+    full_name = first_name + " " + last_name
+    length_first_name = len(first_name)
+    length_surname = len(last_name)
+    length_full_name = len(full_name)
+    return length_first_name, length_surname, length_full_name, full_name
+
+
+def display_length_name():
+    # INPUT
+    user_first_name = input("Please, enter your name: ")
+    user_last_name = input("Enter your last name: ")
+
+    # PROCESSING
+    (
+        length_user_first_name,
+        length_user_last_name,
+        length_full_name,
+        full_name,
+    ) = processing(user_first_name, user_last_name)
+
+    # OUTPUT
+    print(f"Your name has {length_user_first_name} characters in it.")
+    print(f"Your surname has {length_user_last_name} characters in it.")
+    print(f"Your full name is {full_name} and has {length_full_name} characters in it")
+
+
+if __name__ == "__main__":
+    display_length_name()
---
+++ tests/more_string_manipulation/test_challenge_080.py
@@ -0,0 +1,24 @@
+# MORE STRING MANIPULATION
+
+from exercises.more_string_manipulation import challenge_080
+
+
+def test_processing():
+    # Define inputs
+    test_first_name = "Marco"
+    test_last_name = "Barr"
+
+    # invoke function with the test inputs
+    (
+        length_user_first_name,
+        length_user_last_name,
+        length_full_name,
+        full_name,
+    ) = challenge_080.processing(test_first_name, test_last_name)
+
+    # verify that function output is as expected
+    assert length_user_first_name == 5
+    assert length_user_last_name == 4
+    assert length_full_name == 10
+    assert full_name == "Marco Barr"
+
