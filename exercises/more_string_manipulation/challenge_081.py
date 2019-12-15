2d85e355a77ca49977cba43dca556b1313d2c858
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-12-15 00:23:31 +0000

    test for empty string on challenge 81

--- exercises/more_string_manipulation/challenge_081.py
+++ exercises/more_string_manipulation/challenge_081.py
@@ -11,6 +11,7 @@ def processing(subject):
     # PROCESSING
     letters = list(subject)
     joined_subject = "-".join(letters) + "-"
+
     return joined_subject
 
 
@@ -23,9 +24,3 @@ def favourite_subject():
 
 if __name__ == "__main__":
     favourite_subject()
-
-
-# ORIGINAL CODE
-# subject = input("Enter your favourire school subject: ")
-# for letter in subject:
-#     print(letter, end="-")
--- tests/more_string_manipulation/test_challenge_081.py
+++ tests/more_string_manipulation/test_challenge_081.py
@@ -10,18 +10,9 @@ def test_favourite_subject():
     return
 
 
-# def test_favourite_object_empty_string():
-#     test_subject = ""
-#     test_output = challenge_081.processing(test_subject)
-#     assert test_output == ""
-#     return
-
-
-# invoke function with the test inputs
-# if test_ob
-
-# verify that function output is as expected
-
-# raises exceptions
-# https://www.youtube.com/watch?v=etosV2IWBF0
+def test_favourite_object_empty_string():
+    test_subject = ""
+    test_output = challenge_081.processing(test_subject)
+    assert test_output == "-"
+    return
 
