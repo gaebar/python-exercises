5aa44efcb1264495dfa3bb8a62f5e1b6dae2a9b2
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-12-19 12:09:17 +0000

    Parametrised test challenge 82

--- exercises/more_string_manipulation/challenge_082.py
+++ exercises/more_string_manipulation/challenge_082.py
@@ -8,6 +8,9 @@ and ending point. Display the characters between those two points.
 
 
 def trim_characters(poem, start, end):
+    length_poem = len(poem)
+    if length_poem < start or end > length_poem:
+        raise IndexError("list index out of range")
     user_line_choose = poem[start:end]
     return user_line_choose
 
--- tests/more_string_manipulation/test_challenge_082.py
+++ tests/more_string_manipulation/test_challenge_082.py
@@ -1,12 +1,43 @@
+import pytest
 from exercises.more_string_manipulation import challenge_082
 
+# create a list and then call it
+testdata = [
+    ("All the world's a stage", 3, 10, " the wo"),
+    ("This is a poem", 1, 2, "h"),
+    ("This is another poem", 2, 2, ""),
+]
 
-def test_trim_characters():
-    poem = "All the world's a stage"
-    start = 3
-    end = 10
+testdata_fails = [
+    ("All the world's a stage", "hey", "j", TypeError),
+    ("", "hey", 2, TypeError),
+    ("Poem", 19, 23, IndexError),
+]
+
+
+@pytest.mark.parametrize("poem, start, end, expected", testdata)
+def test_trim_characters(poem, start, end, expected):
+    # poem = "All the world's a stage"
+    # start = 3
+    # end = 10
 
     trimmed_poem = challenge_082.trim_characters(poem, start, end)
 
-    assert trimmed_poem == " the wo"
+    assert trimmed_poem == expected
 
+
+@pytest.mark.parametrize("poem, start, end, expected", testdata_fails)
+def test_trim_characters_fails(poem, start, end, expected):
+
+    with pytest.raises(expected):
+        challenge_082.trim_characters(poem, start, end)
+
+
+# def test_trim_characters(poem, start, end, expected):
+# poem = "All the world's a stage"
+# start = 3
+# end = 10
+
+# trimmed_poem = challenge_082.trim_characters(poem, start, end)
+
+# assert trimmed_poem == expected
