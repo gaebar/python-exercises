5dc66def0cf8976be704e6d94270d058dbab2318
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-12-30 20:06:38 +0100

    reading and writing to file challenges

---
+++ exercises/reading_and_writing_from_a_text_file/challenge_106.py
@@ -0,0 +1,15 @@
+"""
+Create a new file called “Names.txt”. Add five names to the document, 
+which are stored on separate lines. Once you have  run the program, 
+look in the location where your program is  stored and check that the 
+file has been created properly. 
+"""
+
+
+file = open("Names.txt", "w")
+file.write("Timothy\n")
+file.write("Tom\n")
+file.write("Gemma\n")
+file.write("Sarah\n")
+file.write("Bob\n")
+file.close()
