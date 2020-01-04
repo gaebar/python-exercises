e54398e45b69c6913b8ff77bdc1f2d89d68cb155
Gaetano Barreca <gaetanobarreca@gmail.com>
2020-01-04 17:12:04 +0100

    refactored challenge 120 and the test

--- exercises/subprograms/challenge_120.py
+++ exercises/subprograms/challenge_120.py
@@ -18,6 +18,9 @@ suitable message.
 """
 
 import random
+import operator
+
+OPERATIONS = {"+": operator.add, "-": operator.sub}
 
 
 def get_user_input_integer(message):
@@ -32,13 +35,8 @@ def ask_for_user_input():
     return get_user_input_integer("Your answer: ")
 
 
-def check_answer_addition(num_1, num_2, user_answer):
-    correct_answer = num_1 + num_2
-    return (user_answer == correct_answer, correct_answer)
-
-
-def check_answer_subtraction(num_1, num_2, user_answer):
-    correct_answer = num_1 - num_2
+def check_answer(num_1, num_2, operation, user_answer):
+    correct_answer = OPERATIONS.get(operation)(num_1, num_2)
     return (user_answer == correct_answer, correct_answer)
 
 
@@ -49,20 +47,12 @@ def display_response(is_answer_correct, correct_answer):
         print(f"Incorrect, the answer was {correct_answer}")
 
 
-def process_addition():
-    num_1 = random.randint(5, 20)
-    num_2 = random.randint(5, 20)
-    print(f"{num_1} + {num_2} =")
+def process(min1, max1, min2, max2, operation):
+    num_1 = random.randint(min1, max1)
+    num_2 = random.randint(min2, max2)
+    print(f"{num_1} {operation} {num_2} =")
     user_answer = ask_for_user_input()
-    return check_answer_addition(num_1, num_2, user_answer)
-
-
-def process_subtraction():
-    num_1 = random.randint(25, 50)
-    num_2 = random.randint(1, 25)
-    print(f"{num_1} - {num_2} =")
-    user_answer = ask_for_user_input()
-    return check_answer_subtraction(num_1, num_2, user_answer)
+    return check_answer(num_1, num_2, operation, user_answer)
 
 
 def challenge_execution():
@@ -71,9 +61,9 @@ def challenge_execution():
     user_input = get_user_input_integer("Enter 1 or 2: ")
 
     if user_input == 1:
-        (is_answer_correct, correct_answer) = process_addition()
+        (is_answer_correct, correct_answer) = process(5, 20, 5, 20, "+")
     elif user_input == 2:
-        (is_answer_correct, correct_answer) = process_subtraction()
+        (is_answer_correct, correct_answer) = process(25, 50, 1, 25, "-")
     else:
         print("Incorrect selection")
         return
--- tests/subprograms/test_challenge_120.py
+++ tests/subprograms/test_challenge_120.py
@@ -2,23 +2,22 @@ import pytest
 
 from exercises.subprograms import challenge_120
 
-TEST_DATA_ADDITION = [(3, 10, 13), (3, 7, 10), (0, 0, 0)]
+TEST_DATA = [(3, 10, "+", 13), (3, 7, "+", 10), (0, 0, "+", 0), (10, 5, "-", 5)]
 
-TEST_DATA_ADDITION_ERROR = [(3, 10, 10), (0, 0, 10)]
+TEST_DATA_ERROR = [(3, 10, "+", 10), (0, 0, "+", 10), (10, 5, "-", 15)]
 
 
-@pytest.mark.parametrize("num_1, num_2, user_answer", TEST_DATA_ADDITION)
-def test_addition(num_1, num_2, user_answer):
-    (check_result, correct_response) = challenge_120.check_answer_addition(
-        num_1, num_2, user_answer
+@pytest.mark.parametrize("num_1, num_2, operation, user_answer", TEST_DATA)
+def test_operation_success(num_1, num_2, operation, user_answer):
+    (check_result, correct_response) = challenge_120.check_answer(
+        num_1, num_2, operation, user_answer
     )
     assert check_result
 
 
-@pytest.mark.parametrize("num_1, num_2, user_answer", TEST_DATA_ADDITION_ERROR)
-def test_addition_error(num_1, num_2, user_answer):
-    (check_result, correct_response) = challenge_120.check_answer_addition(
-        num_1, num_2, user_answer
+@pytest.mark.parametrize("num_1, num_2, operation, user_answer", TEST_DATA_ERROR)
+def test_operation_error(num_1, num_2, operation, user_answer):
+    (check_result, correct_response) = challenge_120.check_answer(
+        num_1, num_2, operation, user_answer
     )
     assert not check_result
-
