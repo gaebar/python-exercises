e0d6e265bf1d99214ec1998b96b5bdaaacf0e694
Gaetano Barreca <gaetanobarreca@gmail.com>
2020-01-07 00:36:30 +0000

    Removed unused code comments

--- exercises/tuples_lists_dictionaries/challenge_073.py
+++ exercises/tuples_lists_dictionaries/challenge_073.py
@@ -32,20 +32,6 @@ def get_user_input_integer(message):
         print("Oops! That was not a valid number. Try again...")
 
 
-## implementation using the for loop index
-# def ask_for_user_foods():
-#     food_dictionary = {}
-
-#     index = 0
-#     for food in food_prompts:
-#         food_dictionary[index] = get_user_input(food)
-#         index += 1
-
-#     return food_dictionary
-
-## implementation using the enumerate built in function
-
-
 def ask_for_user_foods():
     food_dictionary = {}
 
@@ -70,12 +56,6 @@ def remove_unwanted_food(food_dictionary):
 def processing(food_dictionary):
     removed_food_dictionary = remove_unwanted_food(food_dictionary)
     sorted_values = sorted(removed_food_dictionary.values())
-
-    #   Code using for loop
-    #   food_dictionary_sorted = {}
-    #   for counter, food_item in enumerate(sorted_values, 1):
-    #       food_dictionary_sorted[counter] = food_item
-    #   return food_dictionary_sorted
 
     return {counter: food_item for counter, food_item in enumerate(sorted_values, 1)}
 
