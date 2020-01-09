8a5086c1a97a7ca6d66f10fddb1e82233e984a12
Gaetano Barreca <gaetanobarreca@gmail.com>
2020-01-09 00:08:46 +0000

    added while loop for option selection

--- exercises/subprograms/challenge_120.py
+++ exercises/subprograms/challenge_120.py
@@ -1,19 +1,19 @@
 """
-Display the following menu to the user:  1) Addition, 2) Subtraction and Enter
+Display the following menu to the user: 1) Addition, 2) Subtraction and Enter
 1 or 2. If the user enters 1, it should run a subprogram that will generate two
 random numbers between 5 and 20, and ask the user to add them together. Work
 out the correct answer and return both the user’s answer and the correct answer.
 
-If they entered 2 as their selection on the menu, it  should run a subprogram
+If the user entered 2 as their selection on the menu, it should run a subprogram
 that will generate one number between 25 and 50 and another number between 1
-and  25 and ask them to work out num1 minus num2. This way they will not have
+and 25 and ask them to work out num1 minus num2. This way they will not have
 to worry about negative answers. Return both the user’s answer and the correct
 answer.
 
 Create another subprogram that will check if the user’s answer matches
-the actual answer. If it does, display  “Correct”, otherwise display a message
+the actual answer. If it does, display “Correct”, otherwise display a message
 that will say “Incorrect, the answer is” and display the real answer. If they
-do not select a relevant option on the first menu  you should display a
+do not select a relevant option on the first menu you should display a
 suitable message.
 """
 
@@ -37,7 +37,8 @@ def ask_for_user_input():
 
 def check_answer(num_1, num_2, operation, user_answer):
     correct_answer = OPERATIONS.get(operation)(num_1, num_2)
-    return (user_answer == correct_answer, correct_answer)
+    is_correct = user_answer == correct_answer
+    return (is_correct, correct_answer)
 
 
 def display_response(is_answer_correct, correct_answer):
@@ -52,23 +53,25 @@ def process(min1, max1, min2, max2, operation):
     num_2 = random.randint(min2, max2)
     print(f"{num_1} {operation} {num_2} =")
     user_answer = ask_for_user_input()
-    return check_answer(num_1, num_2, operation, user_answer)
+    (is_correct, correct_answer) = check_answer(num_1, num_2, operation, user_answer)
+    display_response(is_correct, correct_answer)
+    return is_correct
 
 
 def challenge_execution():
-    print("1) Addition \n2) Subtraction")
+    while True:
+        user_input = get_user_input_integer(
+            "Press 1 for addition, 2 for subtraction, 3 to terminate the program: "
+        )
 
-    user_input = get_user_input_integer("Enter 1 or 2: ")
-
-    if user_input == 1:
-        (is_answer_correct, correct_answer) = process(5, 20, 5, 20, "+")
-    elif user_input == 2:
-        (is_answer_correct, correct_answer) = process(25, 50, 1, 25, "-")
-    else:
-        print("Incorrect selection")
-        return
-
-    display_response(is_answer_correct, correct_answer)
+        if user_input == 1:
+            process(5, 20, 5, 20, "+")
+        elif user_input == 2:
+            process(25, 50, 1, 25, "-")
+        elif user_input == 3:
+            return
+        else:
+            print("Incorrect selection")
 
 
 if __name__ == "__main__":
