297e4eee32bad6ab525c6b66b32e8938f111e5f7
Gaetano Barreca <gaetanobarreca@gmail.com>
2020-01-09 00:39:16 +0000

    Added tests to challenge 121

--- exercises/subprograms/challenge_118.py
+++ exercises/subprograms/challenge_118.py
@@ -1,7 +1,7 @@
 """
-Define a subprogram that will ask the user to  enter a number and 
-save it as the variable  “num”. Define another subprogram that will  use “num” and count 
-from 1 to that number. 
+Define a subprogram that will ask the user to  enter a number and
+save it as the variable “num”. Define another subprogram that will  use “num” and count
+from 1 to that number.
 """
 
 
